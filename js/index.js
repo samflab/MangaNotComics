cc18135b8739be99a82f49026fc383aca5d6fac5
Masudha Meher <kasturimeh@gmail.com>
2022-02-04 09:58:11 +0530

    add: authentication pages

--- js/index.js
+++ js/index.js
@@ -9,59 +9,64 @@ const closeBtnLogin = document.querySelector(".drawer-close-login");
 const closeBtnPassword = document.querySelector(".drawer-close-password");
 
 const createAccountLink = document.querySelector(".signup-link");
+const signupLink = document.querySelector(".signup-link-login");
 const loginLink = document.querySelector(".login-link");
 const passwordLink = document.querySelector(".password-link");
 
-account.addEventListener("click", () => {
-  accountDrawerLogin.style.width = "30rem";
+const displayLoginDrawer = () => {
+  accountDrawerLogin.style.width = "25rem";
   accountDrawerLogin.style.transition = "transition: 0.5s";
   accountDrawerLogin.style.boxShadow = "-5px 8px 20px 4px gray";
-});
+  closeSignupDrawer();
+};
 
-closeBtnLogin.addEventListener("click", () => {
-  accountDrawerLogin.style.width = "0";
-  accountDrawerLogin.style.transition = "transition: 0.5s";
-  accountDrawerLogin.style.boxShadow = "none";
-});
-
-closeBtnSignup.addEventListener("click", () => {
-  accountDrawerSignup.style.width = "0";
-  accountDrawerSignup.style.transition = "transition: 0.5s";
-  accountDrawerSignup.style.boxShadow = "none";
-});
-
-closeBtnPassword.addEventListener("click", () => {
-  passwordDrawer.style.width = "0";
-  passwordDrawer.style.transition = "0.5s";
-  passwordDrawer.style.boxShadow = "none";
-});
-
-createAccountLink.addEventListener("click", () => {
+const displaySignupDrawer = () => {
   accountDrawerSignup.style.width = "25rem";
   accountDrawerSignup.style.transition = "transition: 0.5s";
   accountDrawerSignup.style.boxShadow = "-5px 8px 20px 4px gray";
 
-  accountDrawerLogin.style.width = "0";
-  accountDrawerLogin.style.transition = "transition: 0.5s";
-  accountDrawerLogin.style.boxShadow = "none";
-});
-
-loginLink.addEventListener("click", () => {
-  accountDrawerLogin.style.width = "25rem";
-  accountDrawerLogin.style.transition = "transition: 0.5s";
-  accountDrawerLogin.style.boxShadow = "-5px 8px 20px 4px gray";
-
-  accountDrawerSignup.style.width = "0";
-  accountDrawerSignup.style.transition = "transition: 0.5s";
-  accountDrawerSignup.style.boxShadow = "none";
-});
-
-passwordLink.addEventListener("click", () => {
+  closeLoginDrawer();
+  closePasswordDrawer();
+};
+const displayPasswordDrawer = () => {
   passwordDrawer.style.width = "25rem";
   passwordDrawer.style.transition = "0.5s";
   passwordDrawer.style.boxShadow = "-5px 8px 20px 4px gray";
+  closeLoginDrawer();
+};
 
+const closeLoginDrawer = () => {
   accountDrawerLogin.style.width = "0";
   accountDrawerLogin.style.transition = "transition: 0.5s";
   accountDrawerLogin.style.boxShadow = "none";
+};
+
+const closeSignupDrawer = () => {
+  accountDrawerSignup.style.width = "0";
+  accountDrawerSignup.style.transition = "transition: 0.5s";
+  accountDrawerSignup.style.boxShadow = "none";
+};
+
+const closePasswordDrawer = () => {
+  passwordDrawer.style.width = "0";
+  passwordDrawer.style.transition = "0.5s";
+  passwordDrawer.style.boxShadow = "none";
+};
+account.addEventListener("click", displayLoginDrawer);
+
+closeBtnLogin.addEventListener("click", closeLoginDrawer);
+
+closeBtnSignup.addEventListener("click", closeSignupDrawer);
+
+closeBtnPassword.addEventListener("click", closePasswordDrawer);
+
+createAccountLink.addEventListener("click", displaySignupDrawer);
+
+loginLink.addEventListener("click", displayLoginDrawer);
+
+passwordLink.addEventListener("click", displayPasswordDrawer);
+
+signupLink.addEventListener("click", () => {
+  displaySignupDrawer();
+  closeLoginDrawer();
 });
