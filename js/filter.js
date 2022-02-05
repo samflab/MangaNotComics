55129940087162e7168f75c9aa2fe76e60df3fe0
Masudha Meher <kasturimeh@gmail.com>
2022-02-05 18:56:42 +0530

    add: rating in filter

--- js/filter.js
+++ js/filter.js
@@ -2,6 +2,7 @@ const filterBtn = document.querySelector(".filter");
 const filterDiv = document.querySelector(".filter-div");
 
 filterBtn.addEventListener("click", () => {
-  if (filterDiv.style.display === "block") filterDiv.style.display = "none";
-  else filterDiv.style.display = "block";
+  if (filterDiv.style.display === "flex") {
+    filterDiv.style.display = "none";
+  } else filterDiv.style.display = "flex";
 });
